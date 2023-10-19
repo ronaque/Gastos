2c2dce3fcc804a2173d1ee08c3f2198f6391772d
Gabriel Bassani <gabriel_bassani@ufrn.edu.br>
2023-10-18 21:52:33 -0300

    adicao dos retângulso de movimentação financeira

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,123 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gastos/theme.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home(), theme: AppTheme().getAppTheme()));
+  runApp(MyApp());
 }
 
-class Home extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _HomeState createState() => _HomeState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FinanceApp(),
+    );
+  }
 }
 
-class _HomeState extends State<Home> {
-  TextEditingController pesoController = TextEditingController();
-  TextEditingController alturaController = TextEditingController();
-  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+class FinanceApp extends StatefulWidget {
+  @override
+  _FinanceAppState createState() => _FinanceAppState();
+}
 
-  void _resetCampos() {
-    pesoController = TextEditingController();
-    alturaController = TextEditingController();
-    _formKey.currentState!.reset();
+class _FinanceAppState extends State<FinanceApp>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Theme(
-        data: AppTheme().getAppTheme(),
-        child: Scaffold(
-            appBar: AppBar(
-                title: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 8.0,0, 0),
-                      child: Text("Controle de Finanças"),
-                    ),
-                    Icon(Icons.account_circle_rounded, size: 35)
-                  ],
-                ),
-              bottom: const TabBar(
-                tabs: [
-                  Tab(text: "Mês"),
-                  Tab(text: "Resumo")
-                ],
-              ),
-              ),
-          body: TabBarView(
-            children: [
-              Text("mes"),
-              Text("resumo")
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MobiFin'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              // Navegue para a tela de perfil aqui.
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(text: 'Mês'),
+              Tab(text: 'Resumo'),
             ],
           ),
-            )
-    ));
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                // Conteúdo da aba "Mês"
+                ListView(
+                  children: [
+                    // Exemplo de registro de movimentação financeira
+                    FinanceEntry(isExpense: true, amount: -50.0),
+                    FinanceEntry(isExpense: false, amount: 100.0),
+                  ],
+                ),
+
+                // Conteúdo da aba "Resumo"
+                Center(
+                  child: Text('Resumo'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FinanceEntry extends StatelessWidget {
+  final bool isExpense;
+  final double amount;
+
+  FinanceEntry({required this.isExpense, required this.amount});
+
+  @override
+  Widget build(BuildContext context) {
+    Color amountColor = isExpense ? Colors.red : Colors.green;
+    String transactionType = isExpense ? 'Gasto' : 'Ganho';
+
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(0, 2),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(transactionType),
+          Text(
+            '\$${amount.toStringAsFixed(2)}',
+            style: TextStyle(color: amountColor),
+          ),
+        ],
+      ),
+    );
   }
 }
